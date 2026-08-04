a71b61b447dc085ae2f2f845b012ef64afa1efa8
RamitShapkota <shapkotaramit47@gmail.com>
2026-08-04 12:31:20 +0545

    feat: Add initial HTML and JavaScript files demonstrating undefined vs not defined concepts

---
+++ NamasteDev/04_Undefefined vs not defined/undefined.js
@@ -0,0 +1,19 @@
+var a;
+console.log(a); // undefined => this act as a place holder before value assigned to a, and it takes memory space, this is not equal to null
+
+console.log(x); // this is not defined because it doesnot allocate memory, it shows not defined error
+
+//js is called weakly typed language 
+
+var a = 10;
+console.log(a);
+
+var a = "hello ramit";
+console.log(a);
+
+// here in same variable a we can assign int and string vaue both thats why js is called weakly typed language
+
+a = undefined;// this mistake we shouldnot make 
+console.log(a);
+
+
