11610bb0c9e1febafa6247a9f2316f08d1b16c56
RamitShapkota <shapkotaramit47@gmail.com>
2026-08-07 22:52:32 +0545

    feat: Add variable examples with let and var, including hoisting explanations

--- NamasteDev/06_Variable let and const/variable.js
+++ NamasteDev/06_Variable let and const/variable.js
@@ -1,3 +1,4 @@
+/*
 // var
 var a = 10;
 a = 20; // ✅ can change value
@@ -15,3 +16,12 @@ const c = 10;
 // c = 20; // ❌ Error: cannot change value
 // const c = 30; // ❌ Error: cannot declare again
 console.log("const =", c);
+
+*/
+
+console.log(b); // it can be access before it initialize
+console.log(a); //let also hoisted like var but it cannot access before some value are assigned to it because of temperal dead zone, to access the value of a we need to first initialize the value to a.
+
+let a = 10;
+var b = 100;
+
